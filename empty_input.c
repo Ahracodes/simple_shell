c5589acf96b861b240b326f39e8816e2e95aa28d
ItsKjaja <contact.kjaj@gmail.com>
2023-05-23 10:04:25 -0700

    exit&check

--- empty_input.c
+++ empty_input.c
@@ -1,6 +1,39 @@
 #include "main.h"
 
-/** endoffile_handler */
+/** endoffile_handler - buffer is endoffile
+ * @data: string
+ * Return: void
+ */
 
 
-/* whitespace_handler */
+
+
+void endoffile(char *data)
+{
+	if(data != NULL)
+	{
+		free(data);
+		data = NULL;
+	}
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	exit(0);
+}
+
+
+/* whitespace_handler 
+@c: string
+Return: O or 1*/
+int whitespace(char *c)
+
+	int x;
+	char y;
+
+	for (x = 0; c[x] != '\0'; x++)
+{
+	y = c[x];
+	if (!isspace(y))
+		return (0);
+}
+return (1)
+	}
